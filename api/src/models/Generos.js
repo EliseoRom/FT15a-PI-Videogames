b22d49705701198bcb86bb3221783948cb471b29
EliseoRom <romeroelisejoaquin@gmail.com>
2021-07-31 15:50:11 -0300

    Model

---
+++ api/src/models/Generos.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+// Exportamos aqui una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('generos', {
+    // nombre de los tipos de generenos de games 
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+    
+  });
+  
+};
