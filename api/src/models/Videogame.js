80447955ba7c7f383281f3a13ccfe9ea472bf074
EliseoRom <romeroelisejoaquin@gmail.com>
2021-07-31 15:33:37 -0300

    Sabado

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    
+    id: {
+      //creo la ID que sea de tipo UUID para generar un numero randown y no se pisen 
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull:false,
+      primaryKey: true
+
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+
+    },
+    background_image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
   });
 };
