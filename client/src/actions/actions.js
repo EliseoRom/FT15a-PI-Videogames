f553d6bba5f463ad474737dc9760a01bb99c6825
EliseoRom <romeroelisejoaquin@gmail.com>
2021-08-18 17:33:56 -0300

    Filtro Genre

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -15,7 +15,7 @@ export function getVideoGames() {
             
         })
     }
-} //  search name function
+} //............SEARCH NAME FUNCTION..............
 export function searchByName(name) {
     return  function (dispatch) {
        return axios.get(`http://localhost:3001/videogames?name=${name}`)
@@ -25,13 +25,7 @@ export function searchByName(name) {
              payload: response.data,
          })
     })}
-} // detalles game nueva /ruta
-// export function getVideogameDetail(id) {
-//     return async function (dispatch) {
-//         const response = await axios.get(`http://localhost:3001/videogame/${id}`); 
-//         dispatch({ type: GET_VIDEOGAME_DETAIL, payload: response.data });  // sino funciona sacar data
-//     } minuto 14 repaso
-// }   // con async await // con promesas 
+} //...............DETAIL GAME....................
 export function getVideogameDetail(id){
     return function (dispatch) {
         return fetch(`http://localhost:3001/videogame/${id}`)
@@ -41,48 +35,97 @@ export function getVideogameDetail(id){
         });
     };
 }
-//  aca 1        filtrado x  genre o genres
-export function filterByGenres(genres) {
-    return { type: 'FILTER_BY_GENRES', payload: genres };
+//  aca 1 ..............FILTER GENRE..................
+export function filterByGenres(payload) {
+    return { type: 'FILTER_BY_GENRES', payload };
 }
-// Aca 2 todos existentes o agregados
-export function filterOrigin(origin) {
-    return { type: 'FILTER_ORIGIN', payload: origin };
-} // aca 3  name
-export function filterName(order) {
-    return { 
-        type: 'FILTER_NAME',
-         payload: order
-    };
-} // aca 4 Raiting 
-export function sortByRating(order) {
-    return { type: 'BY_RATING', payload: order };
-} // POST CREATE GAME
-// export function postGame(payload) {
-//     return async function(dispatch) {
-//         const post = await axios.get('http://localhost:3001/videogame', payload)
+// Aca 2...............FILTER ORIGIN..................
+// return { type: 'FILTER_ORIGIN', payload: origin };
+export function filterOrigin(value) {
+    if (value === 'DB') {
+		return {
+			type: 'DB',
+		};
+	} else if (value === 'API'){
+		return {
+			type: 'API',
+		};
+	}else if (value === 'ALL') {
+		return {
+            type: 'ALL'
+        }
+} 
+}
+// aca 3 ...................FILTER NAME................
+export function filterName(value) {
+       if (value === 'ORDER_ASC') {
+            return {
+                type: 'ORDER_ASC',
+            };
+        } else {
+            return {
+                type: 'ORDER_DESC',
+            };
+        }
+    }
+   
+ // aca 4.................FILTER RAITING................
+export function sortByRating(value) {
+    if (value === 'RAITING_MAX') {
+        return {
+            type: 'RAITING_MAX',
+        };
+    } else {
+        return {
+            type: 'RAITING_MIN',
+        };
+    }
+    // return { type: 'BY_RATING', payload: order };
+} 
 
-//     }
+//.................... POST CREATE GAME.................
+export function postGame(payload) {
+    return async function(dispatch) {
+        const post = await axios.get('http://localhost:3001/videogame', payload)
+        return {
+            type:POST_GAME, 
+            post
+        }
+    }
+}
+// export function createVideogame(obj) {
+//     return (dispatch) =>
+//         axios.post('http://localhost:3001/videogame', obj)
+//         .then((res) => {
+//           dispatch({type: CREATE_VIDEOGAME, payload: res.data});
+//         });
 // }
-export function postVideogame(payload){ //le paso un payload ya que trae data
+
+export function getGenres(){
     return async function(dispatch){
-      return await axios.post("http://localhost:3001/videogame" , payload);
+      const info = await axios.get("http://localhost:3001/genres");
+    return dispatch({
+      type: GET_GENRE,
+      payload: info.data
+    });
    }
   }
 
-
-
-//  4  GAME FILTER EXPORT
-export const FILTER_ORIGIN = 'FILTER_ORIGIN';
+//   TODOS LOS FILTROS
 export const FILTER_BY_GENRES = 'FILTER_BY_GENRES';
-export const FILTER_NAME = 'FILTER_NAME';
-export const BY_RATING = 'BY_RATING';
-
-export const POST_GAME = "POST_GAME";
+export const ORDER_ASC = "ORDER_ASC";
+export const ORDER_DESC = 'ORDER_DESC';
+export const RAITING_MAX = 'RAITING_MAX';
+export const RAITING_MIN = 'RAITING_MIN';
+export const GET_GENRE ='GET_GENRE';
+export const DB = "DB";
+export const ALL = "ALL";
+export const API = "API";
+// Create Game
+export const CREATE_VIDEOGAME = "CREATE_VIDEOGAME";
 export const GET_VIDEOGAMES = 'GET_VIDEOGAMES';
-
+export const  POST_GAME = "POST_GAME";
+// SERCH 
 export const SEARCH_BY_NAME = 'SEARCH_BY_NAME';
-
+// DETAIL GAME
 export const GET_VIDEOGAME_DETAIL = 'GET_VIDEOGAME_DETAIL';
-
-// export const GET_VIDEOGAMES_DETAIL = 'GET_VIDEOGAMES_DETAIL';
